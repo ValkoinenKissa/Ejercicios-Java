f10724c57c2d0c125fcfc36c07fe57a70bff7da3
Alberto Bollo <albertobollo7@protonmail.com>
2024-11-04 05:58:36 +0100

    Actualización de ejercicios

---
+++ DAM_2024/Ejercicios/src/primer_trimestre/MayorYMenor_2_Numeros.java
@@ -0,0 +1,27 @@
+package primer_trimestre;
+
+import java.util.Scanner;
+
+/*
+ *Dado nos números por teclado, que me muestre cuál es el mayor, menor y si son iguales.
+ */
+public class MayorYMenor_2_Numeros {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int num1, num2;
+        System.out.print("Ingrese el primer numero: ");
+        num1 = sc.nextInt();
+        System.out.print("Ingrese el segundo numero: ");
+        num2 = sc.nextInt();
+
+        if (num1 > num2) {
+            System.out.println("El num1: " + num1 + " es mayor que el num2: " + num2);
+        } else if (num2 > num1) {
+            System.out.println("El num2: " + num2 + " es mayor que el num1: " + num1);
+
+        } else {
+            System.out.println("num1 y num2 son iguales!!");
+        }
+        sc.close();
+    }
+}
