1e2dedc19c993b93dbe5235b7b97bcfbf9a583e8
Alberto Bollo <albertobollo7@protonmail.com>
2024-12-17 03:08:23 +0100

    Nuevos ejercicios

--- DAM_2024/Ejercicios/src/primer_trimestre/ejercicio_concesionario/Concesionario_vehiculos.java
+++ DAM_2024/Ejercicios/src/primer_trimestre/ejercicio_concesionario/Concesionario_vehiculos.java
@@ -56,6 +56,8 @@ public class Concesionario_vehiculos {
 
         } while (!opcion.equals("5"));
 
+        sc.close();
+
     }
 
     public static void mostrarVehiculos(String[][] matrizVehiculos) {
