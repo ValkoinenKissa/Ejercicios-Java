bb4b077dda9d3e472974bf3fa46c403a421ba92e
Alberto Bollo <albertobollo7@protonmail.com>
2025-01-11 18:48:28 +0100

    Nuevos ejercicios

--- DAM_2024/Ejercicios/src/segundo_trimestre/ejercicio_biblioteca/Biblioteca.java
+++ DAM_2024/Ejercicios/src/segundo_trimestre/ejercicio_biblioteca/Biblioteca.java
@@ -1,5 +1,28 @@
 package segundo_trimestre.ejercicio_biblioteca;
+/*
+Realiza una aplicación para la gestión de la biblioteca de la Universidad Europea.
 
+En la biblioteca se tienen libros, revistas y películas que se pueden
+alquilar durante un determinado tiempo por parte de los clientes.
+
+La aplicación debe ofrecer un menú al empleado para poder realizar las siguientes tareas:
+
+1.- Insertar artículo.
+
+2.- Eliminar artículo.
+
+3.- Modificar artículo.
+
+4.- Alquilar artículo.
+
+5.- Devolución del artículo
+
+6.- Salir.
+
+Todos aquellos datos que no facilita el enunciado, debéis decidirlos vosotras/os.
+
+Mucha suerte
+ */
 public class Biblioteca {
     public static void main(String[] args) {
 
