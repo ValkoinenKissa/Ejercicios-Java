da15854ad5d9058e898823816e9daa5612fb4394
Alberto Bollo <albertobollo7@protonmail.com>
2025-01-20 23:02:53 +0100

    Nuevos ejercicios

--- DAM_2024/Ejercicios/src/segundo_trimestre/poo/ejercicio_banco/Ejecutable.java
+++ DAM_2024/Ejercicios/src/segundo_trimestre/poo/ejercicio_banco/Ejecutable.java
@@ -7,7 +7,7 @@ Crea una clase llamada Cuenta que tendrá los siguientes atributos: DNI del titu
 
 El DNI será obligatorio y el saldo es opcional. Crea dos constructores que cumpla lo anterior.
 
-Crea sus métodos get, set y toString (éste último lo veremos más adelante.).
+Crea sus métodos get, set y toString (este último lo veremos más adelante.).
 
 Tendrá dos métodos especiales:
 
@@ -25,65 +25,145 @@ public class Ejecutable {
     public static void main(String[] args) {
         //Declaracion de variables
         Scanner sc = new Scanner(System.in);
-        double saldoDisponible;
         int opcion;
-        String dni;
+        Cuenta [] listadoCuentas = new Cuenta[2];
         //Declaracion de clase
-        Cuenta Cuenta;
+        Cuenta cuenta;
 
-        opcion = mostrarMenu(sc);
 
-        switch (opcion) {
-            case 1:
-                System.out.println("1 - Abrir cuenta corriente");
-                break;
-            case 2:
-                System.out.println("2 - Realizar un ingreso en cuenta");
-                break;
-            case 3:
-                System.out.println("3 - Realizar un reintegro");
-                break;
-            case 4:
-                System.out.println("4 - Realizar una transferencia bancaria");
-                break;
-            case 5:
-                System.out.println("5 - Agregar una nueva cuenta bancaria a la lista de contactos");
-                break;
-            case 6:
-                System.out.println("6 - Eliminar una cuenta bancaria exixtente de la lista de contactos");
-                break;
-            case 7:
-                System.out.println("Hasta pronto, recuerde que tiene a disposicion " +
-                        " la banca online las 24h del dia");
-                break;
+        do {
+
+            opcion = mostrarMenu(sc);
+
+
+            switch (opcion) {
+                case 1:
+                    System.out.println("Abrir cuenta");
+                    break;
+                case 2:
+                    System.out.println("2 - Realizar un ingreso en cuenta");
+                    break;
+                case 3:
+                    System.out.println("3 - Realizar un reintegro");
+                    break;
+                case 4:
+                    System.out.println("4 - Realizar una transferencia bancaria");
+                    break;
+                case 5:
+                    System.out.println("5 - Agregar una nueva cuenta bancaria a la lista de contactos");
+                    break;
+                case 6:
+                    System.out.println("6 - Eliminar una cuenta bancaria exixtente de la lista de contactos");
+                    break;
+                case 7:
+                    System.out.println("Hasta pronto, recuerde que tiene a disposicion " + " la banca online las 24h del dia");
+                    break;
+            }
+
+        }while (opcion != 7);
+
+    }
+
+    /// ///////////////////////////////////////////////////////////////////////////////////
+
+    public static void abrirCuenta(Scanner sc, Cuenta [] listadoCuentas) {
+        String dni;
+        double saldo;
+        dni = introducirDNI(sc);
+        saldo = saldoInicial(sc);
+
+        for (int i = 0; i < listadoCuentas.length; i++ ) {
+            Cuenta cuenta = new Cuenta(dni, saldo);
         }
 
     }
 
-    public static int mostrarMenu(Scanner sc) {
-        int opcion;
-        boolean stop = false;
-
+    public static double saldoInicial(Scanner sc) {
+        double saldo = 0;
+        boolean stop = true;
         do {
-            System.out.println("Ingresa el numero de la operacion que quieres efectuar: ");
-            opcion = sc.nextInt();
-            System.out.println("1 - Abrir cuenta corriente");
-            System.out.println("2 - Realizar un ingreso en cuenta");
-            System.out.println("3 - Realizar un reintegro");
-            System.out.println("4 - Realizar una transferencia bancaria");
-            System.out.println("5 - Agregar una nueva cuenta bancaria a la lista de contactos");
-            System.out.println("6 - Eliminar una cuenta bancaria exixtente de la lista de contactos");
-            System.out.println("7 - Salir");
 
-            if (opcion < 1 || opcion > 7) {
-                System.out.println("La opcion que has introducido no es correcta, la opcion debe de estar entre" +
-                        " 1 y 7");
+            System.out.println("Introduzca el saldo inicial que tendra la cuenta, si quieres abir la cuenta con " +
+                    " 0 euros, introduce un 0 ");
+            if (sc.hasNextDouble()) {
+                saldo = sc.nextDouble();
+
+                if (saldo < 0) {
+                    System.out.println("El saldo no puede ser negativo, introduce un saldo positivo: ");
+                    stop = false;
+                } else {
+                    System.out.println("Saldo registrado correctamente: " + saldo);
+                    stop = true;
+                }
+            }else {
+                System.out.println("Entrada no válida. Por favor, introduce un número válido.");
+            }
+
+        } while (!stop);
+
+        return saldo;
+    }
+
+
+
+    ///  //////////////////////////////////////////////////////////////////////////////////
+
+    public static String introducirDNI(Scanner sc) {
+        String dni;
+        boolean stop;
+        sc.nextLine();
+        do {
+            System.out.println("Introduzca su DNI (Obligatorio): ");
+            dni = sc.nextLine();
+
+            if (dni.isEmpty()) {
+                System.out.println("Es campo del DNI esta en blanco, introducelo de nuevo: ");
+                stop = false;
+            } else {
+                System.out.println("DNI resgistrado correctamente " + dni);
                 stop = true;
             }
 
         } while (!stop);
 
-        return opcion;
+        return dni;
+    }
 
+    /// //////////////////////////////////////////////////////////
+
+    public static int mostrarMenu(Scanner sc) {
+        int opcion;
+        mostarOpciones();
+        opcion = solicitarOpcion(sc);
+        return opcion;
+    }
+
+    /// /////////////////////////////////////////////////////////
+
+    public static void mostarOpciones() {
+        System.out.println("1 - Abrir cuenta corriente");
+        System.out.println("2 - Realizar un ingreso en cuenta");
+        System.out.println("3 - Realizar un reintegro");
+        System.out.println("4 - Realizar una transferencia bancaria");
+        System.out.println("5 - Agregar una nueva cuenta bancaria a la lista de contactos");
+        System.out.println("6 - Eliminar una cuenta bancaria exixtente de la lista de contactos");
+        System.out.println("7 - Salir");
+    }
+
+    /// /////////////////////////////////////////////////////////
+
+    public static int solicitarOpcion(Scanner sc) {
+        int opcion;
+        do {
+
+            System.out.println("Ingresa el numero de la operacion que quieres efectuar: ");
+            opcion = sc.nextInt();
+            if (opcion < 1 || opcion > 7) {
+                System.out.println("La opcion no es coreecta. Debe estar entre 1 y 7, intentalo de nuevo.");
+            }
+
+        } while (opcion < 1 || opcion > 7);
+
+        return opcion;
     }
 }
