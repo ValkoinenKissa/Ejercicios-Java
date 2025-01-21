926aea2f2f5cdd11e3914be4c06a457ceec6be82
Alberto Bollo <albertobollo7@protonmail.com>
2025-01-21 22:29:12 +0100

    Nuevos ejercicios

--- DAM_2024/Ejercicios/src/segundo_trimestre/poo/ejercicio_banco/Ejecutable.java
+++ DAM_2024/Ejercicios/src/segundo_trimestre/poo/ejercicio_banco/Ejecutable.java
@@ -26,9 +26,7 @@ public class Ejecutable {
         //Declaracion de variables
         Scanner sc = new Scanner(System.in);
         int opcion;
-        Cuenta [] listadoCuentas = new Cuenta[2];
-        //Declaracion de clase
-        Cuenta cuenta;
+        Cuenta[] listadoCuentas = new Cuenta[5];
 
 
         do {
@@ -38,43 +36,105 @@ public class Ejecutable {
 
             switch (opcion) {
                 case 1:
-                    System.out.println("Abrir cuenta");
+                    abrirCuenta(sc, listadoCuentas);
                     break;
                 case 2:
-                    System.out.println("2 - Realizar un ingreso en cuenta");
+                    System.out.println("2 . Eliminar una cuenta banciaria");
                     break;
                 case 3:
-                    System.out.println("3 - Realizar un reintegro");
+                    mostarCuentas(listadoCuentas);
                     break;
                 case 4:
-                    System.out.println("4 - Realizar una transferencia bancaria");
+                    System.out.println("4 . Realizar un ingreso en cuenta");
                     break;
                 case 5:
-                    System.out.println("5 - Agregar una nueva cuenta bancaria a la lista de contactos");
+                    System.out.println("5 . Realizar un reintegro");
                     break;
                 case 6:
-                    System.out.println("6 - Eliminar una cuenta bancaria exixtente de la lista de contactos");
+                    System.out.println("6 . Realizar una transferencia bancaria");
                     break;
                 case 7:
                     System.out.println("Hasta pronto, recuerde que tiene a disposicion " + " la banca online las 24h del dia");
                     break;
             }
 
-        }while (opcion != 7);
+        } while (opcion != 7);
 
+    }
+
+    /// ///////////////////////////////////////////////////////////////////////
+
+    public static int comprobarEspacio(Cuenta[] listadoCuentas) {
+        for (int i = 0; i < listadoCuentas.length; i++) {
+            if (listadoCuentas[i] == null) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    /// ///////////////////////////////////////////////////////////////////////
+    public static void comprobarDniCliente(Scanner sc, Cuenta[] listadoCuentas) {
+        boolean stop = true;
+        String decision;
+        String dniCliente = sc.nextLine();
+
+        do {
+            for (Cuenta listadoCuenta : listadoCuentas) {
+                if (listadoCuenta != null && listadoCuenta.getDni().equals(dniCliente)) {
+                    listadoCuenta.setDni(dniCliente);
+                } else {
+                    System.out.println("El dni del cliente introducido ya existe en el sistema. " +
+                            " ¿Quieres intentarlo de nuevo? (S/N)");
+                    decision = sc.nextLine().toUpperCase();
+                    if (decision.equals("S")) {
+                        stop = false;
+                    } else if (decision.equals("N")) {
+                        System.out.println("Volviendo al menu principal...");
+                        stop = true;
+                    } else {
+                        System.out.println("Opción incorrecta, solo puedes introducir (S o N)");
+                        stop = false;
+                    }
+                }
+            }
+        } while (!stop);
+    }
+
+    /// ////////////////////////////////////////////////////////////////////////////////////
+
+    public static void mostarCuentas(Cuenta[] listadoCuentas) {
+        if (listadoCuentas == null) {
+            System.out.println("El listado de cuentas es null.");
+            return;
+        }
+
+        for (Cuenta listadoCuenta : listadoCuentas) {
+            if (listadoCuenta == null) {
+                System.out.println("Cuenta no inicializada (null).");
+            } else {
+                System.out.println(listadoCuenta);
+            }
+        }
     }
 
     /// ///////////////////////////////////////////////////////////////////////////////////
 
-    public static void abrirCuenta(Scanner sc, Cuenta [] listadoCuentas) {
+    public static void abrirCuenta(Scanner sc, Cuenta[] listadoCuentas) {
         String dni;
+        int indice;
         double saldo;
-        dni = introducirDNI(sc);
-        saldo = saldoInicial(sc);
-
-        for (int i = 0; i < listadoCuentas.length; i++ ) {
-            Cuenta cuenta = new Cuenta(dni, saldo);
-        }
+            indice = comprobarEspacio(listadoCuentas);
+            if (indice != -1) {
+                dni = introducirDNI(sc);
+                comprobarDniCliente(sc, listadoCuentas);
+                saldo = saldoInicial(sc);
+                listadoCuentas[indice] = new Cuenta(dni, saldo);
+                System.out.println("Cuenta creada correctamente, mostrando detalles a continuación: ");
+                System.out.println(listadoCuentas[indice].toString());
+            }else {
+                System.out.println("No hay suficiente espacio en el sistema para procesar una nueva cuenta");
+            }
 
     }
 
@@ -95,7 +155,7 @@ public class Ejecutable {
                     System.out.println("Saldo registrado correctamente: " + saldo);
                     stop = true;
                 }
-            }else {
+            } else {
                 System.out.println("Entrada no válida. Por favor, introduce un número válido.");
             }
 
@@ -105,14 +165,13 @@ public class Ejecutable {
     }
 
 
-
     ///  //////////////////////////////////////////////////////////////////////////////////
 
     public static String introducirDNI(Scanner sc) {
         String dni;
         boolean stop;
-        sc.nextLine();
         do {
+            sc.nextLine();
             System.out.println("Introduzca su DNI (Obligatorio): ");
             dni = sc.nextLine();
 
@@ -142,11 +201,11 @@ public class Ejecutable {
 
     public static void mostarOpciones() {
         System.out.println("1 - Abrir cuenta corriente");
-        System.out.println("2 - Realizar un ingreso en cuenta");
-        System.out.println("3 - Realizar un reintegro");
-        System.out.println("4 - Realizar una transferencia bancaria");
-        System.out.println("5 - Agregar una nueva cuenta bancaria a la lista de contactos");
-        System.out.println("6 - Eliminar una cuenta bancaria exixtente de la lista de contactos");
+        System.out.println("2 - Eliminar una cuenta corriente");
+        System.out.println("3 - Mostar todas las cuentas bancarias del sistema");
+        System.out.println("4 - Realizar un ingreso en cuneta");
+        System.out.println("5 - Realizar un reintegro");
+        System.out.println("6 - Realizar una transferencia");
         System.out.println("7 - Salir");
     }
 
