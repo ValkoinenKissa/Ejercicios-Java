9b17d624f3e0dc3c96b2cce3e35ca7a2aedcb05f
Alberto Bollo <albertobollo7@protonmail.com>
2025-01-25 18:25:14 +0100

    Nuevos ejercicios

---
+++ DAM_2024/Ejercicios/src/segundo_trimestre/depuracion_entornos/Multiplica.java
@@ -0,0 +1,12 @@
+package segundo_trimestre.depuracion_entornos;
+/*
+Modificar el programa anterior, incluyendo un bucle que muestre la tabla de multiplicar del 5 del 1 al 10
+(bucle for con el contador de la variable i).
+ */
+public class Multiplica {
+    public static void main(String[] args) {
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("5 X " + i + " = " + (5 * i));
+        }
+    }
+}
