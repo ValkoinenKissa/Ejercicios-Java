75497b03c61b322876dda4937d594d0626924748
Alberto Bollo <albertobollo7@protonmail.com>
2025-02-07 21:34:03 +0100

    Nuevos ejercicios

---
+++ DAM_2024/Ejercicios/src/segundo_trimestre/ejercicio_electrodomesticos/Ejecutable.java
@@ -0,0 +1,38 @@
+package segundo_trimestre.ejercicio_electrodomesticos;
+/*
+Crearemos una clase llamada Electrodomestico con las siguientes características:
+
+Sus atributos son precio base, color, consumo energético (letras entre A y F) y peso.
+Por defecto, el color sera blanco, el consumo energético sera F, el precioBase es de 100 € y
+el peso de 5 kg. Usa constantes para ello.
+Los colores disponibles son blanco, negro, rojo, azul y gris. No importa si el nombre esta en mayúsculas o en minúsculas.
+Los constructores que se implementaran serán
+Un constructor por defecto.
+Un constructor con el precio y peso. El resto por defecto.
+Un constructor con todos los atributos.
+Los métodos que implementara serán:
+Métodos get de todos los atributos.
+comprobarConsumoEnergetico(char letra): comprueba que la letra es correcta, sino es correcta usara la letra por defecto.
+Se invocara al crear el objeto y no sera visible.
+comprobarColor(String color): comprueba que el color es correcto, sino lo es usa el color por defecto.
+Se invocara al crear el objeto y no sera visible.
+precioFinal(): según el consumo energético, aumentara su precio, y según su tamaño, también. Esta es la lista de precios:
+Letra	Precio
+A	100 €
+B	80 €
+C	60 €
+D	50 €
+E	30 €
+F	10 €
+Tamaño	Precio
+Entre 0 y 19 kg	10 €
+Entre 20 y 49 kg	50 €
+Entre 50 y 79 kg	80 €
+Mayor que 80 kg	100 €
+
+ */
+public class Ejecutable {
+    public static void main(String[] args) {
+
+    }
+}
