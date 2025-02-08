0a8a132c65cca90f21612658558fa32b5da2056b
Alberto Bollo <albertobollo7@protonmail.com>
2025-02-08 21:31:16 +0100

    Nuevos ejercicios

---
+++ DAM_2024/Ejercicios/src/segundo_trimestre/ejercicio_electrodomesticos/ConsumoEnergetico.java
@@ -0,0 +1,5 @@
+package segundo_trimestre.ejercicio_electrodomesticos;
+
+public enum ConsumoEnergetico {
+    A, B, C, D, E, F
+}
---
+++ DAM_2024/Ejercicios/src/segundo_trimestre/ejercicio_electrodomesticos/Electrodomestico.java
@@ -0,0 +1,17 @@
+package segundo_trimestre.ejercicio_electrodomesticos;
+/*
+Crearemos una clase llamada Electrodomestico con las siguientes características:
+
+Sus atributos son precio base, color, consumo energético (letras entre A y F) y peso.
+Por defecto, el color sera blanco, el consumo energético sera F, el precioBase es de 100 € y
+el peso de 5 kg. Usa constantes para ello.
+Los colores disponibles son blanco, negro, rojo, azul y gris. No importa si el nombre esta en mayúsculas o en minúsculas.
+Los constructores que se implementaran serán
+Un constructor por defecto.
+Un constructor con el precio y peso. El resto por defecto.
+Un constructor con todos los atributos.
+ */
+public class Electrodomestico {
+    double precioBase;
+    String color;
+}
