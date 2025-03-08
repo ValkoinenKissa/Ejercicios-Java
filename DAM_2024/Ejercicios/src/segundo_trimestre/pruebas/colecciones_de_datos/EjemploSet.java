d1ad743db709ed793fadda6fc2f85d785bee5d79
Alberto Bollo <albertobollo7@protonmail.com>
2025-03-08 21:47:56 +0100

    Clase prueba

---
+++ DAM_2024/Ejercicios/src/segundo_trimestre/pruebas/colecciones_de_datos/EjemploSet.java
@@ -0,0 +1,35 @@
+package segundo_trimestre.pruebas.colecciones_de_datos;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+import java.util.TreeSet;
+
+public class EjemploSet {
+    public static void main(String[] args) {
+        Set<Integer> hashSet = new HashSet<>();
+        long tiempoInicial = System.currentTimeMillis();
+        for (int i = 0; i < 1_000_000; i++) {
+            hashSet.add(i);
+        }
+        long tiempoFinal = System.currentTimeMillis();
+        System.out.println("Tiempo empleado por HashSet: " + (tiempoFinal - tiempoInicial));
+
+        Set<Integer> treeSet = new TreeSet<>();
+        tiempoInicial= System.currentTimeMillis();
+        for (int i= 0; i < 1_000_000; i++) {
+            treeSet.add(i);
+        }
+        tiempoFinal = System.currentTimeMillis();
+        System.out.println("Tiempo empleado por TreeSet: " + (tiempoFinal - tiempoInicial));
+
+        Set<Integer> linkedHashSet = new LinkedHashSet<>();
+        tiempoInicial= System.currentTimeMillis();
+        for (int i= 0; i < 1_000_000; i++) {
+            linkedHashSet.add(i);
+        }
+        tiempoFinal = System.currentTimeMillis();
+        System.out.println("Tiempo empleado por LinkedHashSet: " + (tiempoFinal - tiempoInicial));
+
+    }
+}
