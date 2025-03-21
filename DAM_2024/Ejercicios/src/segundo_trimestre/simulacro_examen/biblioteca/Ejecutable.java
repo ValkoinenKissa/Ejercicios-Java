1b3d5439f8dd0f0b6d27c2d7a6c533b81db3dc8c
Alberto Bollo <albertobollo7@protonmail.com>
2025-03-21 22:50:29 +0100

    Ejercicio simulacro

---
+++ DAM_2024/Ejercicios/src/segundo_trimestre/simulacro_examen/biblioteca/Ejecutable.java
@@ -0,0 +1,20 @@
+package segundo_trimestre.simulacro_examen.biblioteca;
+/*
+Tenéis que realizar una aplicación para llevar a cabo la gestión de una biblioteca.
+Dicha aplicación, la usan los trabajadores para la gestión de artículos y clientes
+
+1.- Dar de alta un usuario, siempre y cuando, no exista ya.
+
+2.- Dar de alta un artículo. Los artículos pueden ser libros, revistas y películas.
+
+3.- Hacer un pŕestamo
+
+4.- Salir
+
+Tiempo maximo 30 mins
+ */
+public class Ejecutable {
+    public static void main(String[] args) {
+
+    }
+}
