0040bbe6786baebff01a1e0f7c4bef720f692c9e
Alberto Bollo <albertobollo7@protonmail.com>
2025-03-22 17:02:25 +0100

    TestSuite added

--- DAM_2024/TestsJunit/tests/TestSuite.java
+++ DAM_2024/TestsJunit/tests/TestSuite.java
@@ -1,10 +1,8 @@
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
-
 @RunWith(Suite.class)
 @Suite.SuiteClasses({Test1Test.class, Test2Test.class})
 
-public class NewTestsSuite {
+public class TestSuite {
 }
-
