2d1ed26d7704c5d4357c9424ab52b83f839ed2fc
Alberto Bollo <albertobollo7@protonmail.com>
2025-04-26 20:55:27 +0200

    clase de ejemplo con select

--- DAM_2024/Ejercicios/src/tercer_trimestre/bbdd_pruebas/clases/select.java
+++ DAM_2024/Ejercicios/src/tercer_trimestre/bbdd_pruebas/clases/select.java
@@ -20,18 +20,22 @@ public class select {
             Connection con = (Connection) DriverManager.getConnection(bdd, usr, pwd);
             // Creamos un objeto de tipo sentencia a partir de la conexión
             Statement stmt = con.createStatement();
-            // Ejecutar la consulta y obtener la información resultado
-            ResultSet rs = stmt.executeQuery("SELECT nombreDirector, edadDirector FROM director");
+            // Ejecutar la consulta y obtener la información de los directores estadounidenses
+            ResultSet rs = stmt.executeQuery("SELECT nombreDirector, edadDirector FROM mcgrawpeliculas.director where nacionalidadDirector = 1");
+            // Mientras haya registros que leer...
             while (rs.next()) {
-                String nombreDirector = rs.getString("nombreDirector");
-                int edadDirector = rs.getInt("edadDirector");
+                // Obtener el valor del campo nombre del registro actual
+                String nombreDirector = rs.getString(1);
+                // Obtener el valor del campo edad del registro actual
+                int edadDirector = rs.getInt(2);
+                // Imprimir la información por pantalla
                 System.out.println("Director: " + nombreDirector + ", Edad: " + edadDirector);
             }
             // Cerrar la conexión
             con.close();
         } catch (SQLException e) {
             // En caso de producirse un error, informar al usuario
-            System.out.println("No se ha podido establecer la conexión con la BDD.");
+            System.out.println("No se ha podido establecer la conexión con la BBDD.");
         }
     }
 }
