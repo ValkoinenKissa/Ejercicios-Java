ea922000c340a13a029b4a7ce5fccecbc5dc8e8c
Alberto Bollo <albertobollo7@protonmail.com>
2025-05-03 03:25:36 +0200

    Nueva interfaz

--- DAM_2024/HelloFX/src/main/java/com/example/hellofx/controller/HelloController.java
+++ DAM_2024/HelloFX/src/main/java/com/example/hellofx/controller/HelloController.java
@@ -2,13 +2,33 @@ package com.example.hellofx.controller;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
+    private Label mensajeLabel;
+
+    @FXML
+    private TextField userTextField;
+
+    @FXML
+    private PasswordField passwordField;
+
 
     @FXML
     protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+        String usuario = userTextField.getText();
+        String contrasena = passwordField.getText();
+        if (usuario.isEmpty() || contrasena.isEmpty()) {
+            mensajeLabel.setText("Por favor, rellena todos los campos.");
+        } else {
+            mensajeLabel.setText("Usuario: " + usuario + " / Contraseña: " + contrasena);
+            // Aquí podrías hacer validación real o login
+        }
+
     }
+
+
+
 }
